b0f43ad3b26db14e97ae53fa4651a9799834e123
lily <a0976198586@gmail.com>
2022-11-14 16:08:17 +0800

    Update 2022-11-14T08:08:16.207Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -8,7 +8,17 @@ var API = 'https://hexschool.github.io/js-filter-data/data.json';
 var data;
 var lists = [];
 var filteredLists = []; // DOM Element
-//const form;
+
+var form = document.querySelector('[data-priceNet-form]'); //讀取文字的方式綁定
+
+var input = document.querySelector('[data-priceNet-input]');
+var resetBtn = document.querySelector('[data-priceNet-reset]');
+var searchInfo = document.querySelector('[data-priceNet-searchInfo]');
+var info = document.querySelector('[data-priceNet-info]');
+var order = document.querySelector('[data-priceNet-order]');
+var type = document.querySelector('[data-priceNet-type]');
+var table = document.querySelector('[data-priceNet-table]');
+var advanceSort = document.querySelector('[data-priceNet-advanceSort]'); // State
 
 function getData() {
   axios.get(API).then(function (response) {

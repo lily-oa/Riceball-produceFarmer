2cb419dbf9915405cd6a47eb2f7d6abb8b2a1acf
lily <a0976198586@gmail.com>
2022-11-14 16:07:58 +0800

    update

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -9,7 +9,18 @@ let lists = [];
 let filteredLists = [];
 
 // DOM Element
-//const form;
+const form = document.querySelector('[data-priceNet-form]'); //讀取文字的方式綁定
+const input = document.querySelector('[data-priceNet-input]'); 
+const resetBtn = document.querySelector('[data-priceNet-reset]'); 
+const searchInfo = document.querySelector('[data-priceNet-searchInfo]'); 
+const info = document.querySelector('[data-priceNet-info]'); 
+const order = document.querySelector('[data-priceNet-order]'); 
+const type = document.querySelector('[data-priceNet-type]'); 
+const table = document.querySelector('[data-priceNet-table]'); 
+const advanceSort = document.querySelector('[data-priceNet-advanceSort]'); 
+
+// State
+
 
 function getData(){
   axios.get(API)

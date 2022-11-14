cd81238ba2f4a7ebe1f795a4def5c6b61e877b94
lily <a0976198586@gmail.com>
2022-11-14 15:41:38 +0800

    Update 2022-11-14T07:41:36.803Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,5 +1,22 @@
-// * 搜尋(currentSearch)、排序(currentOrder)、種類(currentType)之間的交叉搜尋
-// * 清除查詢按鈕 (resetBtn)
-// * 搜尋結果現況展示 (info)
 "use strict";
+
+//搜尋(currentSearch)、排序(currentOrder)、種類(currentType)之間的交叉搜尋
+//清除查詢按鈕 (resetBtn)
+//搜尋結果現況展示 (info)
+// data
+var API = 'https://hexschool.github.io/js-filter-data/data.json';
+var data;
+var lists = [];
+var filteredLists = []; // DOM Element
+//const form;
+
+function getData() {
+  axios.get(API).then(function (response) {
+    data = response.data.filter(function (item) {
+      return item.作物名稱;
+    });
+  });
+}
+
+getData();
 //# sourceMappingURL=all.js.map

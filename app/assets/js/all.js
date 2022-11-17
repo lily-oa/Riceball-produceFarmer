b9ebafbf230ecc8fc3a7bd79f890983d54183e7b
lily <a0976198586@gmail.com>
2022-11-18 06:53:16 +0800

    update

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,6 @@
+import "regenerator-runtime/runtime";
+import "core-js/stable";
+
 //搜尋(currentSearch)、排序(currentOrder)、種類(currentType)之間的交叉搜尋
 //清除查詢按鈕 (resetBtn)
 //搜尋結果現況展示 (info)
@@ -36,6 +39,7 @@ function getData(){
 getData();
 
 // ---------------------學習 Promise Async Await----------
+
 
 
 // promise

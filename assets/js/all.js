4f51a6b0d5fe717f8ac96dc9fe27f442a8191f3d
lily <a0976198586@gmail.com>
2022-11-18 02:47:44 +0800

    Update 2022-11-17T18:47:42.344Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,5 +1,9 @@
 "use strict";
 
+function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
+
+function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
+
 //搜尋(currentSearch)、排序(currentOrder)、種類(currentType)之間的交叉搜尋
 //清除查詢按鈕 (resetBtn)
 //搜尋結果現況展示 (info)
@@ -71,16 +75,50 @@ function checkAward(data) {
       }
     }, 1000);
   });
-}
+} // correctHomework('小明')
+//   .then(data => checkAward(data))
+//   .then(reword => console.log(reword))
+//   .catch(error => console.log(error));
+// async 、 await 非同步的寫法
 
-correctHomework('小明').then(function (data) {
-  return checkAward(data);
-}).then(function (reword) {
-  return console.log(reword);
-})["catch"](function (error) {
-  return console.log(error);
-}); // async 、 await 非同步的寫法
 
-var studentA = correctTest('小明');
-var rewardA = checkReward(studentA);
+var init = /*#__PURE__*/function () {
+  var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee() {
+    var studentA, rewardA;
+    return regeneratorRuntime.wrap(function _callee$(_context) {
+      while (1) {
+        switch (_context.prev = _context.next) {
+          case 0:
+            _context.prev = 0;
+            _context.next = 3;
+            return correctHomework('小明');
+
+          case 3:
+            studentA = _context.sent;
+            _context.next = 6;
+            return checkAward(studentA);
+
+          case 6:
+            rewardA = _context.sent;
+            _context.next = 12;
+            break;
+
+          case 9:
+            _context.prev = 9;
+            _context.t0 = _context["catch"](0);
+            console.log(_context.t0);
+
+          case 12:
+          case "end":
+            return _context.stop();
+        }
+      }
+    }, _callee, null, [[0, 9]]);
+  }));
+
+  return function init() {
+    return _ref.apply(this, arguments);
+  };
+}(); // const studentA = correctHomework('小明');
+// const rewardA = checkAward(studentA);
 //# sourceMappingURL=all.js.map
